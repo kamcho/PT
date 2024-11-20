d14bae797370f47ebcb0e33442420f3470333058
kevingitundu <kevingitundu@gmail.com>
2024-11-20 11:44:37 +0300

    openai

--- callback.py
+++ callback.py
@@ -23,7 +23,6 @@ def call_url():
 
 
 while True:
-    # call_url()
     headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
