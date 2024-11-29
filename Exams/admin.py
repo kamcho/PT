7959b79861a496fc12cb39ebfbcb53f0ee7979a1
kevingitundu <kevingitundu@gmail.com>
2024-11-29 14:18:03 +0300

    tokens3

--- Exams/admin.py
+++ Exams/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(TopicalQuizes)
-admin.site.register(TopicalQuizAnswers)
+# admin.site.register(TopicalQuizes)
+# admin.site.register(TopicalQuizAnswers)
 admin.site.register(StudentTest)
 admin.site.register(StudentsAnswers)
 admin.site.register(ClassTest)
