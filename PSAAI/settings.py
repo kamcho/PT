3a0b91634e42ec846a1cc654df8bdd8a1dda0b5a
kevingitundu <kevingitundu@gmail.com>
2025-02-03 15:12:12 +0300

    revamp

--- PSAAI/settings.py
+++ PSAAI/settings.py
@@ -194,7 +194,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
