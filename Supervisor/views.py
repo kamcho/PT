c320593ee6314e5bb6e4d5fa0b95e9b035f1b029
kevingitundu <kevingitundu@gmail.com>
2025-03-17 10:38:43 +0300

    eh

--- Supervisor/views.py
+++ Supervisor/views.py
@@ -62,8 +62,9 @@ class SupervisorHomeView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
         subjects = Subject.objects.all()
         topics = Topic.objects.using('default2').all()
         subtopics = Subtopic.objects.using('default2').all()
-       
-       Topic.objects.create(id=topic.id, subject=subject, name=topic.name, order=topic.order, topics_count=topic.topics_count, test_size=topic.test_size, time=topic.time)
+        # for topic in topics:
+        #     subject = subjects.get(name=topic.subject.name, grade=topic.subject.grade)
+        #     sub = Topic.objects.create(id=topic.id, subject=subject, name=topic.name, order=topic.order, topics_count=topic.topics_count, test_size=topic.test_size, time=topic.time)
         for subtopic in subtopics:
             topic = Topic.objects.get(id=subtopic.topic.id)
             subject = Subject.objects.get(name=subtopic.subject.name, grade=subtopic.subject.grade)
